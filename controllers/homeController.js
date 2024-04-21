f9e3ac2abd32080da1453e04d8f1ab4c5263e1f8
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-04-20 23:01:15 -0600

    Añadiendo peticion de productos

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,7 @@ import { getProductos } from "../models/homeModel.js"
 const home = async (req, res ) => {
     try {
         const productos = await getProductos();
+        console.log(productos);
         res.render('index', {
             productos: productos
         })
