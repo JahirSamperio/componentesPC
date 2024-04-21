ea12cb4ca3be7e26cf14a7b39e1b088c39741be8
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-04-20 21:45:51 -0600

    Estructura de proyecto

---
+++ app.js
@@ -0,0 +1,9 @@
+import {config} from 'dotenv';
+import Server from './server/server.js';
+// Ejecuta la función config() para cargar las variables de entorno desde el archivo .env
+config();
+
+const server = new Server();
+
+//la funcion listen viene de la ckase server
+server.listen();
