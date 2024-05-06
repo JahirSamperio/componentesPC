91b05d390d1a4474c2bf03062f8859b5c7f77a95
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-05-06 10:10:45 -0600

    Carrito listo

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,27 +1,45 @@
-import {agregar, eliminar} from '../models/cartModel.js'
+import { agregar, eliminar, buscarProducto } from '../models/cartModel.js'
 import jwt from 'jsonwebtoken';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 config();
 
-const agregarCarrito = async(req, res) => {
+const agregarCarrito = async (req, res) => {
     try {
-        const {_token} = req.cookies;
+        const { _token } = req.cookies;
 
         let tokenId;
         let autenticado;
-        if(_token){
-            autenticado=true;
+        if (_token) {
+            autenticado = true;
             const token = jwt.verify(_token, process.env.JWT_SECRET);
             tokenId = token.id;
         } else {
-            autenticado=false
+            autenticado = false
         }
-        const {usuario_id, ensamblado_id} = req.params;
 
-        await agregar(usuario_id, ensamblado_id);
+        //Agregar
+        const { usuario_id, ensamblado_id } = req.params;
+        let cantidad = 1;
+        //Buscar si ya existe
+        const existente = await buscarProducto(usuario_id, ensamblado_id)
+        if (existente.length > 0) {
+            let cantidadArray = existente[0].cantidad;
+            let id = existente[0].id;
+            console.log(existente);
+
+            if (typeof cantidadArray === 'number' && !isNaN(cantidadArray)) {
+                cantidadArray += 1;
+            } 
+            await agregar(usuario_id, ensamblado_id, cantidadArray, id);
+        } else if(existente.length === 0){
+            await agregar(usuario_id, ensamblado_id, cantidad);
+        }
+
+
 
         res.status(200).json({
             msg: "Agregado exitosamente"
+
         })
     } catch (error) {
         console.log(error);
@@ -33,7 +51,7 @@ const agregarCarrito = async(req, res) => {
 
 const eliminarCarrito = async (req, res) => {
     try {
-        const {id_favorito} = req.params
+        const { id_favorito } = req.params
 
         await eliminar(id_favorito)
 
--- models/cartModel.js
+++ models/cartModel.js
@@ -2,17 +2,32 @@ import { conexion } from "../db/conexion.js";
 
 
 //Selecciona informacion del inmueble
-function agregar(usuario_id, ensamblado_id) {
-    return new Promise((resolve, reject) => {
-        conexion.query(
-            `INSERT INTO cart(usuario_id, ensamblado_id)
-            VALUES ('${usuario_id}', '${ensamblado_id}')`,
-            function (error, result, field) {
-                if (error) 
-                    return reject(error);
-                return resolve(result);
-            })
-    })
+function agregar(usuario_id, ensamblado_id, cantidad, id) {
+    console.log(cantidad);
+    if(!id){
+        return new Promise((resolve, reject) => {
+            conexion.query(
+                `INSERT INTO cart(usuario_id, ensamblado_id, cantidad)
+                VALUES ('${usuario_id}', '${ensamblado_id}', '${cantidad}')`,
+                function (error, result, field) {
+                    if (error) 
+                        return reject(error);
+                    return resolve(result);
+                })
+        })
+    } else {
+        return new Promise((resolve, reject) => {
+            conexion.query(
+                `UPDATE cart
+                SET usuario_id = '${usuario_id}', ensamblado_id = '${ensamblado_id}', cantidad = '${cantidad}'
+                WHERE id = '${id}'`,
+                function (error, result, field) {
+                    if (error) 
+                        return reject(error);
+                    return resolve(result);
+                })
+        })
+    }
 }
 
 function cart(usuario_id){
@@ -44,8 +59,25 @@ function eliminar(id){
     })
 }
 
+function buscarProducto(usuario_id, ensamblado_id){
+    return new Promise((resolve, reject) => {
+        conexion.query(
+            `SELECT * FROM cart
+            WHERE cart.ensamblado_id = '${ensamblado_id}'
+            AND cart.usuario_id = '${usuario_id}'
+            LIMIT 1;`,
+            function (error, result, field) {
+                if (error) 
+                    return reject(error);
+                return resolve(result);
+            })
+    })
+}
+
+
 export {
     agregar,
     cart,
-    eliminar
+    eliminar,
+    buscarProducto
 }
